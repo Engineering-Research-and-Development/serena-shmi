dce96a07e74c178fe493e56cdca7d77c726f4936
Manfredi Pistone <manfredi.pistone@gmail.com>
2020-01-17 15:53:35 +0100

    new nifi endpoint

--- shmi-mdinterface/config/index.js
+++ shmi-mdinterface/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  nifiUrl: process.env.NODE_ENV === 'production' ? "http://serena:9093" : "http://10.10.10.163:9081",
+  nifiUrl: process.env.NODE_ENV === 'production' ? "http://serena:9093" : "http://localhost:9081",
   myenvvar: process.env.NODE_ENV === 'production' ? process.env.MYENV : 'myenv'
 };
