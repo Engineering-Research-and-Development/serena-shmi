9042eef576798aa73504f24a4665a8093fad4b70
Manfredi Pistone <manfredi.pistone@gmail.com>
2020-04-09 17:45:39 +0200

    minor improvements

--- shmi-dashboard/config/index.js
+++ shmi-dashboard/config/index.js
@@ -2,10 +2,9 @@ module.exports = {
   stage: process.env.NODE_ENV,
   port: process.env.NODE_ENV === 'production' ? process.env.PORT :  8080,
   localMetadataApiUrl:  process.env.NODE_ENV === 'production' 
-    ? "http://"+process.env.WEB_SERVER_HOST+":"+process.env.WEB_SERVER_PORT+"/api/metadata/api/1.0"
+    ? "http://"+process.env.VUE_APP_WEB_SERVER_HOST+":"+process.env.VUE_APP_WEB_SERVER_PORT+"/api/metadata/api/1.0"
     : "http://localhost:9090/api/metadata/api/1.0",
   serenaDigestPredictionUrl: process.env.NODE_ENV === 'production' 
-    ? "http://"+process.env.WEB_SERVER_HOST+":"+process.env.WEB_SERVER_PORT+"/api/metadata/api/1.0/digest_prediction"
-    : "http://localhost:9090/api/metadata/api/1.0/digest_prediction", 
-  jsonPlaceholder: "https://jsonplaceholder.typicode.com"
+    ? "http://"+process.env.VUE_APP_WEB_SERVER_HOST+":"+process.env.VUE_APP_WEB_SERVER_PORT+"/api/metadata/api/1.0/digest_prediction"
+    : "http://localhost:9090/api/metadata/api/1.0/digest_prediction"
 };
