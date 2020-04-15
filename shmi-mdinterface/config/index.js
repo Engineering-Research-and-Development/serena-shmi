f24b39f6eba0334b8fe5aebbe45fc6fae1d95109
Manfredi Pistone <manfredi.pistone@gmail.com>
2020-04-15 17:57:59 +0200

    env renamed

--- shmi-mdinterface/config/index.js
+++ shmi-mdinterface/config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   stage: process.env.NODE_ENV,
-  port: process.env.NODE_ENV === 'production' ? process.env.PORT :  4444,
+  port: process.env.NODE_ENV === 'production' ? process.env.MDIFPORT :  4444,
   nifiUrl: process.env.NODE_ENV === 'production' 
     ? "http://"+process.env.NIFI_HOST+":"+process.env.NIFI_PORT  + "/serena/1.0"
     : "http://serena:9093/serena/1.0",
-    nifiUrl_ws: process.env.NODE_ENV === 'production' 
+  nifiUrl_ws: process.env.NODE_ENV === 'production' 
     ? "http://"+process.env.NIFI_HOST+":"+process.env.NIFI_PORT
     : "http://serena:9093",
   nifiUrl_old: process.env.NODE_ENV === 'production' 
--- shmi-webserver/config/index.js
+++ shmi-webserver/config/index.js
@@ -1,15 +1,11 @@
 module.exports = {
   stage: process.env.NODE_ENV,
-  port: process.env.NODE_ENV === 'production' ? process.env.PORT :  9090,
+  port: process.env.NODE_ENV === 'production' ? process.env.WEBPORT :  9090,
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
   session_secret: process.env.NODE_ENV === 'production' ? process.env.SECRET_SESSION : 'secret',
   mongoUri: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI :  "mongodb://127.0.0.1:27017/shmi",
 
   metadataInterfaceUrl:  process.env.NODE_ENV === 'production' 
   ? "http://"+process.env.MD_INTERFACE_HOST+":"+process.env.MD_INTERFACE_PORT
-  : "http://localhost:4444",
-
-  dashboardUrl:  process.env.NODE_ENV === 'production' 
-  ? "http://"+process.env.DASHBOARD_HOST+":"+process.env.DASHBOARD_PORT
-  : "http://localhost:80",
+  : "http://localhost:4444"
 };
