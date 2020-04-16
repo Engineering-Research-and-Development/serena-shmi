b9a790dfd1e5f096ba682498815d390b4993ff90
Manfredi Pistone <manfredi.pistone@gmail.com>
2020-04-16 15:38:32 +0200

    parametric protocol

--- shmi-dashboard/config/index.js
+++ shmi-dashboard/config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   stage: process.env.NODE_ENV,
   localMetadataApiUrl:  process.env.NODE_ENV === 'production' 
-    ? "http://"+process.env.VUE_APP_WEB_SERVER_HOST+":"+process.env.VUE_APP_WEB_SERVER_PORT+"/api/metadata/api/1.0"
+    ?  process.env.VUE_APP_WEB_SERVER_PROTOCOL+"://"+process.env.VUE_APP_WEB_SERVER_HOST+":"+process.env.VUE_APP_WEB_SERVER_PORT+"/api/metadata/api/1.0"
     : "http://localhost:9090/api/metadata/api/1.0",
   serenaDigestPredictionUrl: process.env.NODE_ENV === 'production' 
-    ? "http://"+process.env.VUE_APP_WEB_SERVER_HOST+":"+process.env.VUE_APP_WEB_SERVER_PORT+"/api/metadata/api/1.0/digest_prediction"
+    ?  process.env.VUE_APP_WEB_SERVER_PROTOCOL+"://"+process.env.VUE_APP_WEB_SERVER_HOST+":"+process.env.VUE_APP_WEB_SERVER_PORT+"/api/metadata/api/1.0/digest_prediction"
     : "http://localhost:9090/api/metadata/api/1.0/digest_prediction"
 };
