e8b15884affba77c693e8bd4a2c6a20996f05963
Vincent <vincentval90@outlook.it>
2020-04-21 18:37:55 +0200

    blind build integrating meas_locations, not tested for DELL services unavailability

--- shmi-dashboard-eng/config/index.js
+++ shmi-dashboard-eng/config/index.js
@@ -29,4 +29,18 @@ module.exports = {
       : "http://localhost:9090/api/metadata/api/1.0/meas_location",
   serenaLabelMlocCalcTypeId: "0000000000000000/6/10",
   serenaRulMlocCalcTypeId: "0000000000000000/6/11",
+  visualization_links: {
+    //Vanno poi inseriti nel config
+    "Punching Tool":
+      "http://serena:9008/SynAreaDashboard/pages/use-case/kone/visualization/combi/index.html",
+    RobotBox:
+      "http://serena:9008/SynAreaDashboard/pages/use-case/comau/visualization/robotbox/index.html",
+  },
+  training_links: {
+    //Vanno poi inseriti nel config
+    "Punching Tool":
+      "http://serena:9008/SynAreaDashboard/pages/use-case/kone/maintenance/combi/index.html",
+    RobotBox:
+      "http://serena:9008/SynAreaDashboard/pages/use-case/comau/maintenance/belt/index.html",
+  }
 };
