b0beac4ba3e247360f78fd666a96866ebd9e4204
Manfredi Pistone <manfredi.pistone@gmail.com>
2020-04-22 19:04:40 +0200

    env for dashboard

--- shmi-dashboard-eng/config/index.js
+++ shmi-dashboard-eng/config/index.js
@@ -2,45 +2,144 @@ module.exports = {
   stage: process.env.NODE_ENV,
   localMetadataApiUrl:
     process.env.NODE_ENV === "production"
-      ? process.env.VUE_APP_WEB_SERVER_PROTOCOL +
+      ? 
+        process.env.VUE_APP_WEB_SERVER_PROTOCOL +
         "://" +
         process.env.VUE_APP_WEB_SERVER_HOST +
         ":" +
         process.env.VUE_APP_WEB_SERVER_PORT +
         "/api/metadata/api/1.0"
-      : "http://localhost:9090/api/metadata/api/1.0",
-  serenaDigestPredictionUrl:
-    process.env.NODE_ENV === "production"
-      ? process.env.VUE_APP_WEB_SERVER_PROTOCOL +
-        "://" +
-        process.env.VUE_APP_WEB_SERVER_HOST +
-        ":" +
-        process.env.VUE_APP_WEB_SERVER_PORT +
-        "/api/metadata/api/1.0/digest_prediction"
-      : "http://localhost:9090/api/metadata/api/1.0/digest_prediction",
+      : 
+        "http://localhost:9090/api/metadata/api/1.0"
+  ,
+
   serenaPredictionUrl:
     process.env.NODE_ENV === "production"
-      ? process.env.VUE_APP_WEB_SERVER_PROTOCOL +
+      ? 
+        process.env.VUE_APP_WEB_SERVER_PROTOCOL +
         "://" +
         process.env.VUE_APP_WEB_SERVER_HOST +
         ":" +
         process.env.VUE_APP_WEB_SERVER_PORT +
         "/api/metadata/api/1.0/meas_location"
-      : "http://localhost:9090/api/metadata/api/1.0/meas_location",
-  serenaLabelMlocCalcTypeId: "0000000000000000/6/10",
-  serenaRulMlocCalcTypeId: "0000000000000000/6/11",
-  visualization_links: {
-    //Vanno poi inseriti nel config
-    "Punching Tool":
-      "http://serena:9008/SynAreaDashboard/pages/use-case/kone/visualization/combi/index.html",
-    RobotBox:
-      "http://serena:9008/SynAreaDashboard/pages/use-case/comau/visualization/robotbox/index.html",
-  },
-  training_links: {
-    //Vanno poi inseriti nel config
-    "Punching Tool":
-      "http://serena:9008/SynAreaDashboard/pages/use-case/kone/maintenance/combi/index.html",
-    RobotBox:
-      "http://serena:9008/SynAreaDashboard/pages/use-case/comau/maintenance/belt/index.html",
-  }
+      : 
+        "http://localhost:9090/api/metadata/api/1.0/meas_location"
+  ,
+  
+  visualization_links: 
+    process.env.NODE_ENV === "production" 
+    ?
+      {
+        "Punching Tool":
+          process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+          "://" +
+          process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+          ":" +
+          process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+          "/syn-vis/pages/use-case/kone/visualization/combi/index.html",
+
+        "RobotBox":
+          process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+          "://" +
+          process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+          ":" +
+          process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+          "/syn-vis/pages/use-case/comau/visualization/robotbox/index.html"
+      }
+    :
+      {
+        "Punching Tool":
+          "http://serena:9008/syn-vis/pages/use-case/kone/visualization/combi/index.html",
+        "RobotBox":
+          "http://serena:9008/syn-vis/pages/use-case/comau/visualization/robotbox/index.html",
+      }
+  ,
+
+  training_links: 
+    process.env.NODE_ENV === "production" 
+    ?
+      {
+        "Punching Tool":
+          process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+          "://" +
+          process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+          ":" +
+          process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+          "/syn-vis/pages/use-case/kone/maintenance/combi/index.html",
+
+        "RobotBox":
+          process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+          "://" +
+          process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+          ":" +
+          process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+          "/syn-vis/pages/use-case/comau/maintenance/belt/index.html"
+      }
+    :
+      {
+        "Punching Tool":
+          "http://serena:9008/syn-vis/pages/use-case/kone/maintenance/combi/index.html",
+        "RobotBox":
+          "http://serena:9008/syn-vis/pages/use-case/comau/maintenance/belt/index.html",
+      }
+  ,
+
+  metadataManager: 
+    process.env.NODE_ENV === "production" 
+    ?
+      process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+      "://" +
+      process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+      ":" +
+      process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+      "/syn-vis/pages/md-manager"
+    :
+      "http://serena:9008/syn-vis/pages/md-manager"
+  ,
+
+  gatewayManager: 
+    process.env.NODE_ENV === "production" 
+    ?
+      process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+      "://" +
+      process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+      ":" +
+      process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+      "/syn-vis/pages/gw-manager"
+    :
+      "http://serena:9008/syn-vis/pages/gw-manager"
+  ,
+
+  politoCharts: 
+    process.env.NODE_ENV === "production" 
+    ?
+      process.env.VUE_APP_SYN_WEB_SERVER_PROTOCOL +
+      "://" +
+      process.env.VUE_APP_SYN_WEB_SERVER_HOST +
+      ":" +
+      process.env.VUE_APP_SYN_WEB_SERVER_PORT +
+      "/syn-vis/pages/charts/silhouette/index.html"
+    :
+      "http://serena:9008/syn-vis/pages/charts/silhouette/index.html"
+,
+        
+rpcaManager: 
+    process.env.NODE_ENV === "production" 
+    ?
+      process.env.VUE_APP_RPCA_MANAGER_URL
+    :
+      "http://serena:9008"
+,
+
+lmsScheduler: 
+  process.env.NODE_ENV === "production" 
+  ?
+    process.env.VUE_APP_LMS_SCHEDULER_URL
+  :
+    "http://serena:9008"
+,
+
+serenaLabelMlocCalcTypeId: "0000000000000000/6/10",
+serenaRulMlocCalcTypeId: "0000000000000000/6/11"
+
 };
